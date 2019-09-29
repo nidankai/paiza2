6cc7b8885f96a6101bd6c116215e1bafe95feffa
nidankai <b4068@oic.jp>
2019-09-29 10:36:33 +0900

    C066:金魚すくい(失敗) https://paiza.jp/career/challenges/318/retry

--- paiza.py
+++ paiza.py
@@ -1,34 +1,31 @@
-today = input().split(' ')
-event_date = input().split(' ')
+input_arg = input().split(' ')
+fish_num = int(input_arg[0])
+poi_num = int(input_arg[1])
+poi_life = int(input_arg[2])
 
-for i in range(len(today)):
-    today[i] = int(today[i])
-for i in range(len(event_date)):
-    event_date[i] = int(event_date[i])
+fish_list = list()
+for i in range(fish_num):
+    fish_list.append(int(input()))
 
-remainder = int(today[0]) % 4
-during_year = 0
-if remainder == 0:
-    during_year = 1
-elif remainder == 1:
-    during_year = 0
-elif remainder == 2:
-    during_year = 3
-elif remainder == 3:
-    during_year = 2
-
-during_month = 0
-if during_year != 0:
-    if today[1] <= event_date[0]:  # 月
-        during_month = 13 * during_year + (event_date[0] - today[1])
+scooped_count = 0
+now_poi_life = poi_life
+poi_count = poi_num
+for fish in fish_list:
+    if fish < now_poi_life:
+        now_poi_life -= fish
+        scooped_count += 1
     else:
-        during_month = 13 * (during_year - 1) + (13 - today[1]) + event_date[0]
-else:
-    during_month = (event_date[0] - today[1])
+        poi_count -= 1
+        if poi_count == 0:
+            break
+        now_poi_life = poi_life
+        if fish < now_poi_life:
+            now_poi_life -= fish
+            scooped_count += 1
+        else:
+            poi_count -= 1
+            if poi_count == 0:
+                break
+            now_poi_life = poi_life
 
-during_day = 0
-if today[2] <= event_date[1]:  # 日
-    during_day = during_month * 13 + (int(today[1]) - int(event_date[0]))
-else:
-    during_day = (during_month - 1) * (13 - int(today[1])) + int(event_date[0])
-print(during_day)
+print(scooped_count)
